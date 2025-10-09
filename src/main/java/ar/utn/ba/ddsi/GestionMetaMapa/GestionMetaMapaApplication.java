911fe9de61adbcda3d22526aa06497f35f0a84a5
NahueeGarcia <nagarcia@frba.utn.edu.ar>
2025-10-09 18:47:10 -0300

    Inicio proyecto

---
+++ src/main/java/ar/utn/ba/ddsi/GestionMetaMapa/GestionMetaMapaApplication.java
@@ -0,0 +1,13 @@
+package ar.utn.ba.ddsi.GestionMetaMapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMetaMapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMetaMapaApplication.class, args);
+	}
+
+}
